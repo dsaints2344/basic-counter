f22bbcb950d399761c3f9696e1ff5ebe5da0f108
dsaints 23444 <ddelossantosmateo@gmail.com>
2020-02-25 16:47:21 -0400

    Increment Event
    Finishing increment event

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 54
+    };
+  };
+
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  };
+
   render() {
+    const {count} = this.state;
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
+        <h1>Count: {count}</h1>
+        <button onClick={this.handleIncrement} className="full-width">Increment</button>
         <button onClick={() => {}} className="full-width">Decrement</button>
         <button onClick={() => {}} className="full-width">Reset</button>
       </section>
