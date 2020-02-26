2738b0004ef5bc9f9ae31f2bdc21a02c1c5d1520
dsaints 23444 <ddelossantosmateo@gmail.com>
2020-02-26 17:09:12 -0400

    Adding decrease and reset

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
-  constructor(){
-    super();
-    this.state = {
-      count: 54
-    };
-  };
+  state = {
+    count: 0
+  }
 
   handleIncrement = () => {
     this.setState({
@@ -14,14 +11,26 @@ export default class Counter extends Component {
     })
   };
 
+  handleDecrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  };
+
+  reset = () => {
+    this.setState({
+      count: 0
+    })
+  }
+
   render() {
     const {count} = this.state;
     return (
       <section className="Counter">
         <h1>Count: {count}</h1>
         <button onClick={this.handleIncrement} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <button onClick={this.handleDecrement} className="full-width">Decrement</button>
+        <button onClick={this.reset} className="full-width">Reset</button>
       </section>
     );
   }
